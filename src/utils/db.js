2ef5952be8d49f377c8e2173a0c41d8734f97b04
NurPro <nurik-31@mail.ru>
2022-05-23 15:16:28 +0500

    zet

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize({
     username: process.env.PG_USER,
     database: process.env.PG_DATABASE,
     host: process.env.PG_HOST,
-    password: process.env.PG_PASSWORD + '',
+    password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
     logging: false
 })
